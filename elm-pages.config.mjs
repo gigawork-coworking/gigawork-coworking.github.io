7c3595fb7cb2fb1ba5aa5808db3d9013ca900f4e
iko <mail@iko.soy>
2025-01-01 06:36:26 +0300

    added yandex metrica

--- elm-pages.config.mjs
+++ elm-pages.config.mjs
@@ -8,7 +8,23 @@ export default {
     return `
 <link rel="stylesheet" href="/style.css" />
 <meta name="generator" content="elm-pages v${context.cliVersion}" />
-  <script src="https://api-maps.yandex.ru/v3/?apikey=ab2d2369-9d42-4b81-8406-ff02533f5fcb&lang=ru_RU"></script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99382050, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/99382050" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 `;
   },
   preloadTagForFile(file) {
